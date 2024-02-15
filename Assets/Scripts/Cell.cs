62c11aed6c8b7e5e6e7e5b35f51396da760cd04f
SkyWatcher <herobrinehater101@gmail.com>
2024-02-15 09:20:47 -0600

    Added Quit Button Functionality

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,23 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Resources;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static Game;
 
 public class Cell : MonoBehaviour
 {
-    private Camera mainCamera = Camera.main;
-    private bool clicked = false;
-
-    private enum state{Empty, X, O, Fire}
+    private Camera mainCamera;
+    private bool _clicked;
+    private string _state;
+    [SerializeField] Image[] _item;
+    [SerializeField] GameObject _gameController;
+    private Game game;
+    private string currentPlayerSymbol;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        _clicked = false;
+        _state = "Empty";
+        _gameController = GameObject.FindGameObjectWithTag("GameController");
+        game = _gameController.GetComponent<Game>();
+        currentPlayerSymbol = game.GetCurrentPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) // 0 represents the left mouse button
+        {
+            // Handle the mouse click
+            HandleMouseClick();
+        }
+    }
+
+    void HandleMouseClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Handle the cell click
+            HandleCellClick(hit.collider.gameObject);
+        }
+    }
+
+    void HandleCellClick(GameObject cell)
+    {
+        if (_state == "Empty")
+        {
+            // Place "X" or "O" on the cell
+            //_state = GameObject.FindGameObjectWithTag("GameController").GetCurrentPlayer();
+
+            // Update the game state (change player turn, check for win/draw, etc.)
+            UpdateGameState();
+        }
+    }
+    
+    void UpdateGameState()
+    {
+        // Implement logic to switch turns, check for win, draw, etc.
+        // This might include calling functions to check win conditions and updating UI.
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Game : MonoBehaviour
 {
+    private bool p1Active;
+    private bool p2Active;
+    private bool winner;
+    private int[] states = new int[]
+    public string currentPlayerSymbol;
+    private GameObject currentPlayer;
+    public bool safeToProceed;
+    [SerializeField] private Cell[] _cells = new Cell[9];
+    [SerializeField] private string[] _itemNames = new string[5];
+    [SerializeField] private Sprite[] _items = new Sprite[5];
+    private Sprite currentItem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        p1Active = false;
+        p2Active = false;
+        safeToProceed = false;
+        currentPlayerSymbol = "";
+        winner = false;
+        currentPlayer = GameObject.FindGameObjectWithTag("Player");
+        currentItem = _items[0];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RunTurn()
+    {
+        if ()
+        {
+            
+        }
+        
+        Debug.Log("Game Started");
+        if (safeToProceed)
+        {
+            if (!p1Active && !p1Active)
+            {
+                p1Active = true;
+                currentPlayerSymbol = "X";
+                Debug.Log("X Turn");
+                currentPlayer.GetComponent<Image>().sprite = _items[1];
+                currentItem = _items[1];
+            }
+            else if (p1Active)
+            {
+                p1Active = false;
+                
+                p2Active = true;
+                currentPlayerSymbol = "O";
+                Debug.Log("O Turn");
+                currentPlayer.GetComponent<Image>().sprite = _items[2];
+                currentItem = _items[2];
+            }
+            else if (p2Active)
+            {
+                p2Active = false;
+                
+                p1Active = true;
+                currentPlayerSymbol = "X";
+                Debug.Log("X Turn");
+                currentPlayer.GetComponent<Image>().sprite = _items[1];
+                currentItem = _items[1];
+            }
+
+            safeToProceed = false;
+        }
+    }
+
+    public string GetCurrentPlayer()
+    {
+        return currentPlayerSymbol;
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+
+    public void Proceed()
+    {
+        safeToProceed = true;
     }
 }
