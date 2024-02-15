eda3fd60641b2d616e30f4c02d7fb0cc3de62871
SkyWatcher <herobrinehater101@gmail.com>
2024-02-14 21:54:05 -0600

    Added Animated Title Background

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    private Camera mainCamera = Camera.main;
+    private bool clicked = false;
+
+    private enum state{Empty, X, O, Fire}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
